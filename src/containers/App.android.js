452e47c8931c5922f2aa3cfd86c279a1aa8a6fc4
Sean <sean.xiao@lianshang.cn>
2016-06-28 17:53:49 +0800

    添加MovieApp

---
+++ src/containers/App.android.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+import MovieApp from '../components/test/MovieApp';
+
+export default class App extends Component {
+    render() {
+        return (
+            <MovieApp />
+        );
+    }
+}
