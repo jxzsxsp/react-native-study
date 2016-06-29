a14dcf48cf56d1d5dfb13daefc123950d2fc88af
Sean <sean.xiao@lianshang.cn>
2016-06-29 18:02:07 +0800

    添加react-native-router-flux示例

--- index.android.js
+++ index.android.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import {AppRegistry} from 'react-native';
-import App from './src/containers/App';
+//import App from './src/containers/App';
+import App from './router_study/example/Example';
 
 AppRegistry.registerComponent('reactNativeStudy', () => App);
